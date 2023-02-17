9b0ea3bf0b575cc2c4cfb84cc62150c16c4d2e26
insomniacc@gmail.com <insomniacc@gmail.com>
2023-02-17 11:58:23 +0000

    Added: basic module files

---
+++ src/PSServiceNow.psm1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = "Stop"
+
+$ModulePath = $PSScriptRoot
+
+$Private = Get-ChildItem (Join-Path $ModulePath Private) -Recurse -Filter *.ps1
+$Public = Get-ChildItem (Join-Path $ModulePath Public) -Recurse -Filter *.ps1
+
+#Dot source the functions
+$Functions = @($Private;$Public).Foreach({
+    [System.IO.File]::ReadAllText($_.FullName, [System.Text.Encoding]::UTF8) + [System.Environment]::NewLine
+})
+. ([System.Management.Automation.ScriptBlock]::Create($Functions))
+
+# Export public functions
+Export-ModuleMember -Function $Public.BaseName -Alias *

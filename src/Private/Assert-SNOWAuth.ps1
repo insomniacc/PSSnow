3b254f2cea3318622fbfef7c6b06ca0866da0041
insomniacc@gmail.com <insomniacc@gmail.com>
2023-02-17 12:00:49 +0000

    Added: Assert-SNOWAuth

---
+++ src/Private/Assert-SNOWAuth.ps1
@@ -0,0 +1,14 @@
+function Assert-SNOWAuth() {
+    [CmdletBinding()]
+    param ()
+    
+    
+    if($null -eq $script:SNOWAuth){
+        Write-Error "Please set ServiceNow authentication with Set-SNOWAuth" -ErrorAction Stop
+    }
+
+    if($script:SNOWAuth.Type -eq "OAuth"){
+        #todo check token expiry, renew if required
+    }
+}
+

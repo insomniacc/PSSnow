448f435e9d43659c5a9705718edc9234bc93e5ca
insomniacc@gmail.com <insomniacc@gmail.com>
2023-03-05 13:57:16 +0000

    Fixed: Typo

--- src/Public/table/New-SNOWObject.ps1
+++ src/Public/table/New-SNOWObject.ps1
@@ -1,13 +1,3 @@
-<#todo 
-    https://docs.servicenow.com/bundle/tokyo-application-development/page/integrate/inbound-rest/concept/c_TableAPI.html
-    sysparm_display_value
-    sysparm_exclude_reference_link
-    sysparm_fields
-    sysparm_view
-    sysparm_query_no_domain
-
-#>
-
 function New-SNOWObject {
     [CmdletBinding(SupportsShouldProcess)]
     param (
@@ -24,12 +14,22 @@ function New-SNOWObject {
         [Parameter()]
         [switch]
         $PassThru,
-        [Parameter(Hidden)]
+        [Parameter(DontShow)]
         [switch]
         $AsBatchRequest
      )
 
     Process {
         Invoke-SNOWTableCREATE -table $Table -Parameters $PSBoundParameters
+
+        <#todo 
+            https://docs.servicenow.com/bundle/tokyo-application-development/page/integrate/inbound-rest/concept/c_TableAPI.html
+            sysparm_display_value
+            sysparm_exclude_reference_link
+            sysparm_fields
+            sysparm_view
+            sysparm_query_no_domain
+
+        #>
     }
 }

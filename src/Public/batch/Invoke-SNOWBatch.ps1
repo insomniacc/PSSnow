dc8e655fe08aa17dea1b7ccf897a3f3f97b25bf4
insomniacc@gmail.com <insomniacc@gmail.com>
2023-03-09 12:10:05 +0000

    Added: Invoke-SNOWBatch Request collection & batch splitting

--- src/Public/batch/Invoke-SNOWBatch.ps1
+++ src/Public/batch/Invoke-SNOWBatch.ps1
@@ -11,6 +11,7 @@ function Invoke-SNOWBatch {
         $BatchSize = 150,
         [Parameter()]
         [int]
+        [ValidateRange(1, 20)]
         $Threads = 1,
         [Parameter()]
         [switch]
@@ -21,36 +22,60 @@ function Invoke-SNOWBatch {
         Assert-SNOWAuth
         $URI = "https://$($script:SNOWAuth.instance).service-now.com/api/now/v2/batch"
         
-        #This will be used as an identifier across all split batches
+        # This will be used as an identifier across split batches
         $BatchGUID = (New-Guid).Guid
 
-        # Get all the batch api supported commands
         if($PSBoundParameters.ContainsKey('ScriptBlock')){
+            $InputScript = $ScriptBlock.ToString()
+            if($InputScript -like "*-AsBatchRequest*"){
+                Throw "The provided scriptblock cannot contain any commands with -AsBatchRequest already specified."
+            }
+
+            # Get a list of all the supported batch commands in the module
             $ModuleCommands = Get-Command -Module "PSServiceNow"
             $SupportedBatchCommands = ($ModuleCommands | Where-Object {$_.Parameters.ContainsKey('AsBatchRequest')}).Name
-        }
-        # If there's need for exceptions they should be added here
-        $ExcludedCommands = @() 
+
+            # Get all PSServiceNow module commands within the scriptblock
+            $ScriptBlockCommands = select-string "(?i)([\w]+-SNOW[\w]+)" -InputObject $InputScript -AllMatches
+
+            # Append the AsBatchRequest parameter to all supported commands in the scriptblock 
+            $ScriptBlockCommands = $ScriptBlockCommands.Matches.value | Sort-Object -Unique | Where-Object {$_ -in $SupportedBatchCommands}
+            $ScriptBlockCommands.Foreach({
+                $InputScript = $InputScript -replace $_,"$_ -AsBatchRequest"
+            })
+            
+            #? Invoke the script and capture the output
+            $Requests = Invoke-Expression -command $InputScript
+        } 
     }
     
     process {
-        #Collect requests
+        #? Calculate how many batches are required
+        $RequestCount = ($Requests | Measure-Object).count
+        if($RequestCount -eq 0){ return }
 
-        switch ($PsCmdlet.ParameterSetName) {
-            "ScriptBlock" {
-                # Get all PSServiceNow module commands within the scriptblock
-                $ScriptBlockCommands = select-string "(?i)([\w]+-SNOW[\w]+)" -InputObject $ScriptBlock.ToString() -AllMatches
-                # Pick out only those that are supported by the Batch API
-                $ScriptBlockCommands = $ScriptBlockCommands.Matches.value | Sort-Object -Unique | Where-Object {$_ -in $SupportedBatchCommands -and $_ -notin $ExcludedCommands}
-
-            }
-            "Requests" {}
-            Default { Write-Error "Parameter set is not defined" }
+        if($RequestCount -gt $BatchSize){
+            $BatchCount = [Math]::Ceiling($RequestCount/$BatchSize)
+        }else{
+            $BatchCount = 1
         }
 
+        #? Split the requests into batches
+        $Batches = [System.Collections.ArrayList]@()
+        $Offset = 0
+        for($i=0;$i -lt $BatchCount;$i++){
+            [void]$Batches.add(
+                [pscustomobject]@{
+                    batch_request_id = "BATCH $i ($BatchGuid)"
+                    rest_requests = ($Requests | Select-Object -first $BatchSize -skip $Offset)
+                }
+            )
+            $Offset += $BatchSize
+        }
 
-        if($PSCmdlet.ShouldProcess("x Requests in x Batches [x Threads]", $PsCmdlet.MyInvocation.InvocationName)){
-            
+        if($PSCmdlet.ShouldProcess("$RequestCount Requests in $BatchCount Batches [$Threads Threads]", $PsCmdlet.MyInvocation.InvocationName)){
+
+           
         }
         
     }

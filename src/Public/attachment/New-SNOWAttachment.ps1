0f9278452c1dfe3fb03925cd9e03147daab06fc8
insomniacc@gmail.com <insomniacc@gmail.com>
2023-03-12 14:24:50 +0000

    Added: New-SNOWAttachment batching & core mime support

--- src/Public/attachment/New-SNOWAttachment.ps1
+++ src/Public/attachment/New-SNOWAttachment.ps1
@@ -43,34 +43,158 @@ function New-SNOWAttachment {
         $Sys_Class_Name,
         [Parameter()]
         [switch]
-        $PassThru
+        $PassThru,
+        [Parameter(DontShow)]
+        [string]
+        $AttachedFilename,
+        [Parameter(DontShow)]
+        [switch]
+        $AsBatchRequest
     )
     
     begin {
         Assert-SNOWAuth
         $BaseURL = "https://$($script:SNOWAuth.instance).service-now.com/api/now/v1/attachment/file"
+        $RestMethod = 'POST'
     }
     
     process {
-        if($PSCmdlet.ShouldProcess($URI,'POST')){   
+        if($AttachedFilename){
+            $Filename = $AttachedFilename
+        }else{
+            $Filename = $File.Name
+        }
+        $URI = "$BaseURL`?file_name=$Filename&table_name=$Sys_Class_Name&table_sys_id=$Sys_ID"
+
+        try{
+            if($PSVersionTable.PSEdition -eq "Core"){
+                #I'm unsure currently of the best method to get the mime type on core.
+                #Setting to a generic octet stream works for most but this could case issues with with classifications, e.g user photo uploads.
+                #For now I've put a simple mapping based on ext
+                $MimeType = switch ($File.Extension) {
+                    '.aac' {'audio/aac'}
+                    '.abw' {'application/x-abiword'}
+                    '.arc' {'application/x-freearc'}
+                    '.avif' {'image/avif'}
+                    '.avi' {'video/x-msvideo'}
+                    '.azw' {'application/vnd.amazon.ebook'}
+                    '.bin' {'application/octet-stream'}
+                    '.bmp' {'image/bmp'}
+                    '.bz' {'application/x-bzip'}
+                    '.bz2' {'application/x-bzip2'}
+                    '.cda' {'application/x-cdf'}
+                    '.csh' {'application/x-csh'}
+                    '.css' {'text/css'}
+                    '.csv' {'text/csv'}
+                    '.doc' {'application/msword'}
+                    '.docx' {'application/vnd.openxmlformats-officedocument.wordprocessingml.document'}
+                    '.eot' {'application/vnd.ms-fontobject'}
+                    '.epub' {'application/epub+zip'}
+                    '.gz' {'application/gzip'}
+                    '.gif' {'image/gif'}
+                    '.htm' {'text/html'}
+                    '.html' {'text/html'}
+                    '.ico' {'image/vnd.microsoft.icon'}
+                    '.ics' {'text/calendar'}
+                    '.jar' {'application/java-archive'}
+                    '.jpeg' {'image/jpeg'}
+                    '.jpg' {'image/jpeg'}
+                    '.js' {'text/javascript'}
+                    '.json' {'application/json'}
+                    '.jsonld' {'application/ld+json'}
+                    '.mid' {'audio/midi'}
+                    '.midi' {'audio/midi'}
+                    '.mjs' {'text/javascript'}
+                    '.mp3' {'audio/mpeg'}
+                    '.mp4' {'video/mp4'}
+                    '.mpeg' {'video/mpeg'}
+                    '.mpkg' {'application/vnd.apple.installer+xml'}
+                    '.odp' {'application/vnd.oasis.opendocument.presentation'}
+                    '.ods' {'application/vnd.oasis.opendocument.spreadsheet'}
+                    '.odt' {'application/vnd.oasis.opendocument.text'}
+                    '.oga' {'audio/ogg'}
+                    '.ogv' {'video/ogg'}
+                    '.ogx' {'application/ogg'}
+                    '.opus' {'audio/opus'}
+                    '.otf' {'font/otf'}
+                    '.png' {'image/png'}
+                    '.pdf' {'application/pdf'}
+                    '.php' {'application/x-httpd-php'}
+                    '.ppt' {'application/vnd.ms-powerpoint'}
+                    '.pptx' {'application/vnd.openxmlformats-officedocument.presentationml.presentation'}
+                    '.rar' {'application/vnd.rar'}
+                    '.rtf' {'application/rtf'}
+                    '.sh' {'application/x-sh'}
+                    '.svg' {'image/svg+xml'}
+                    '.tar' {'application/x-tar'}
+                    '.tif' {'image/tiff'}
+                    '.tiff' {'image/tiff'}
+                    '.ts' {'video/mp2t'}
+                    '.ttf' {'font/ttf'}
+                    '.txt' {'text/plain'}
+                    '.vsd' {'application/vnd.visio'}
+                    '.wav' {'audio/wav'}
+                    '.weba' {'audio/webm'}
+                    '.webm' {'video/webm'}
+                    '.webp' {'image/webp'}
+                    '.woff' {'font/woff'}
+                    '.woff2' {'font/woff2'}
+                    '.xhtml' {'application/xhtml+xml'}
+                    '.xls' {'application/vnd.ms-excel'}
+                    '.xlsx' {'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'}
+                    '.xml' {'application/xml'}
+                    '.xul' {'application/vnd.mozilla.xul+xml'}
+                    '.zip' {'application/zip'}
+                    '.3gp' {'video/3gpp'}
+                    '.3g2' {'video/3gpp2'}
+                    '.7z' {'application/x-7z-compressed'}
+                    default {"application/octet-stream"}
+                }
+            }else{
+                $MimeType = [System.Web.MimeMapping]::GetMimeMapping($File)
+            }
+        }catch{
+            $MimeType = "application/octet-stream"
+        }
+        
+        $Body = [System.IO.File]::ReadAllBytes($File)
+        
+        if($AsBatchRequest.IsPresent){
+            if($URI -match "(?<=service-now.com).*"){
+                return @{
+                    id                       = (new-guid).guid
+                    url                      = $Matches[0]
+                    method                   = $RestMethod
+                    headers                  = @(
+                                                    @{  
+                                                        'name'  = 'Content-Type'
+                                                        'value' = $MimeType
+                                                    },
+                                                    @{ 
+                                                        'name'  = 'Accept'
+                                                        'value' = 'application/json'
+                                                    }
+                                                )
+                    exclude_response_headers = $False
+                    body                     = [convert]::ToBase64String($Body)
+                }
+            }
+        }
+        
+        if($PSCmdlet.ShouldProcess($URI,$RestMethod)){   
             $RestSplat = @{
                 Headers    = @{
                     'Content-Type' = $MimeType
                     'Accept' = 'application/json'
                 }
-                Method     = 'POST'
-                URI        = "$BaseURL`?file_name=$($File.name)&table_name=$Sys_Class_Name&table_sys_id=$Sys_ID"
-                Body       = [System.IO.File]::ReadAllBytes($File)
+                Method     = $RestMethod
+                URI        = $URI
+                Body       = $Body
                 Credential = $Script:SNOWAuth.Credential
             }
 
             if($PSVersionTable.PSEdition -eq "Core"){
                 $RestSplat += @{SkipHeaderValidation = $true}
-                $RestSplat.Headers.'Content-Type' = "application/octet-stream"
-                #I'm unsure currently of the best method to get the mime type on core.
-                #Setting to a generic octet stream works for now, although I'm not sure if this can cause any other impact elsewhere with classifications.
-            }else{
-                $RestSplat.Headers.'Content-Type' = [System.Web.MimeMapping]::GetMimeMapping($File)
             }
 
             $Response = Invoke-RestMethod @RestSplat

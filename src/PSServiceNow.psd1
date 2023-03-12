5873165fd9b68ff0265984a3b6f3d9a87c628322
insomniacc@gmail.com <insomniacc@gmail.com>
2023-03-12 14:25:19 +0000

    Added: Remove-SNOWAttachment

--- src/PSServiceNow.psd1
+++ src/PSServiceNow.psd1
@@ -83,6 +83,7 @@ FunctionsToExport = @(
     'Get-SNOWStats'
     'New-SNOWAttachment'
     'Get-SNOWAttachment'
+    'Remove-SNOWAttachment'
 )
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ src/Public/attachment/Remove-SNOWAttachment.ps1
@@ -0,0 +1,43 @@
+function Remove-SNOWAttachment {
+    <#
+    .SYNOPSIS
+        Removes attachments from servicenow.
+    .DESCRIPTION
+        Deletes an attachment from servicenow.
+    .LINK
+        https://developer.servicenow.com/dev.do#!/reference/api/tokyo/rest/c_AttachmentAPI
+    .EXAMPLE
+        Remove-SNOWAttachment -Sys_ID a73f6d2447292110d3e5fa8bd36d4304
+        Removes a single attachment by sys_id
+    .EXAMPLE
+        Get-SNOWUser -user_name "bruce.wayne" | Get-SNOWAttachment | Remove-SNOWAttachment -Verbose
+        Removes all attachments on the associated parent object
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='High')]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateScript({ $_ | Confirm-SysID -ValidateScript })]
+        [string]
+        $Sys_ID
+    )
+    
+    begin {
+        Assert-SNOWAuth
+        $BaseURL = "https://$($script:SNOWAuth.instance).service-now.com/api/now/v1/attachment/"
+
+        $AuthSplat = @{
+            Credential = $Script:SNOWAuth.Credential
+        }
+    }
+    
+    process {
+        try{
+            if($PSCmdlet.ShouldProcess($URI,'DELETE')){
+                Invoke-RestMethod -URI "$BaseURL$Sys_ID" -Method "DELETE" @AuthSplat
+            }
+        }catch{
+            Write-Error "$($_.Exception.Message) [$URI]"
+        }
+    }
+}

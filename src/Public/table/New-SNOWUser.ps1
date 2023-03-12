b1aec9849c513843255b81af3c6c7cbc6a598777
insomniacc@gmail.com <insomniacc@gmail.com>
2023-03-12 14:26:59 +0000

    Removed: New-SNOWUser Base64 photo type

--- src/Public/table/New-SNOWUser.ps1
+++ src/Public/table/New-SNOWUser.ps1
@@ -87,7 +87,23 @@ function New-SNOWUser {
         [string]
         $street,
         [Parameter()]
-        [string]
+        [ValidateScript({
+            if($_ | Test-Path){
+                if($_ | Test-Path -PathType Leaf){
+                    $filetypes = @('.jpg','.png','.bmp','.gif','.jpeg','.ico','.svg')
+                    if([System.IO.Path]::GetExtension($_) -in $filetypes){
+                        $true
+                    }else{
+                        Throw "Incorrect filetype, must be one of the following: $($filetypes -join ',')"
+                    }
+                }else{
+                    Throw "Filepath cannot be a directory."
+                }
+            }else{
+                Throw "Unable to find file."
+            }
+        })]
+        [System.IO.FileInfo]
         $photo,
         [Parameter()]
         [string]
@@ -104,36 +120,12 @@ function New-SNOWUser {
 
         if($photo){
             <#
-                Adding photos to a user record is done in a second API call to a different endpoint,
-                So technically it's a separate thing all together, but it's something you exact should be available when creating a user
-                I've added it as a feature here for usability.
+                Adding photos to a user record is done in a second API call to a different endpoint (attachment API),
+                It's been added to this function for ease of use.
                 It is not supported by batch API requests in this way, because we need to create the user first to get their sys_id in order to attach the photo.
                 In this instance it would be best to make the calls separately by omitting the photo from New-SNOWUser and also running a separate batch with New-SNOWUserPhoto.
                 If both -AsBatchRequest and -Photo are provided this will be highlighted in a warning message and the photo will not be set.
-            #>
-
-            #? Photo can either be a filepath or base64. We'll check and convert so that the format is always base64
-            $PhotoIsBase64  =   try {
-                                    [Void][System.Convert]::FromBase64String($photo)
-                                    $true
-                                } catch {
-                                    $false
-                                }
-
-            if(-not $PhotoIsBase64){
-                if(Test-Path $Photo){
-                    # As per https://docs.servicenow.com/en-US/bundle/utah-platform-administration/page/administer/field-administration/task/t_UsingImageFields.html
-                    # The image type must be .gif, .jpg/.jpeg, or .png.
-                    if([System.IO.Path]::GetExtension($Photo) -in @('.jpg','.jpeg','.gif','.png')){
-                        $photo = Convert-ImageFileToBase64 -FilePath $photo    
-                    }else{
-                        Write-Error "Photo must be one of the following filetypes: .jpg/.jpeg/.gif/.png"
-                    }
-                }else{
-                        Throw "Photo must be either a filepath or a base64 encoded string."
-                }
-            }            
-
+            #>         
             #? Photo requires a separate rest call, so we'll remove it from the bound parameters
             [void]$PSBoundParameters.Remove('photo')
         }
@@ -142,7 +134,7 @@ function New-SNOWUser {
         $Response = Invoke-SNOWTableCREATE -table $table -Parameters $PSBoundParameters -Passthru
 
         if($Response.sys_id -and $photo){
-            New-SNOWUserPhoto -Base64String $photo -SysID $Response.sys_id
+            New-SNOWUserPhoto -SysID $Response.sys_id -filepath $photo
         }elseif($PSBoundParameters.AsBatchRequest.IsPresent -and $photo){
             Write-Warning "Ignoring 'photo' param. New-SNOWUser does not support the photo parameter while batching.`nPlease make a separate batch call with New-SNOWUserPhoto.`nSee: https://github.com/insomniacc/PSServiceNow/blob/main/docs/Batching_New_User_Photos.MD"
         }

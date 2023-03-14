90c6aa257dac4c178581e5871fb464937eb94e0b
insomniacc@gmail.com <insomniacc@gmail.com>
2023-03-14 11:29:57 +0000

    fixed: Remove-SNOWObject pipe support from other objects

--- src/Public/table/Remove-SNOWObject.ps1
+++ src/Public/table/Remove-SNOWObject.ps1
@@ -20,16 +20,24 @@ function Remove-SNOWObject {
         [ValidateScript({ $_ | Confirm-SysID -ValidateScript })]
         [string]
         $Sys_ID,
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName='Table')]
         [ValidateNotNullOrEmpty()]
         [string]
         $Table,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName='sys_class_name')]
+        [ValidateNotNullOrEmpty()]
+        [alias('ParentSysID')]
+        [string]
+        $sys_class_name,
         [Parameter()]
         [switch]
         $RestrictDomain = $false
     )
 
     Process {
+        if($sys_class_name){
+            $Table = $sys_class_name
+        }
         Invoke-SNOWTableDELETE -table $Table -Parameters $PSBoundParameters
     }
 }

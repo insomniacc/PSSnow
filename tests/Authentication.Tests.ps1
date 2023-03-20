90790694b8f2ee293d7f8cf2bd3da8bcb7e65f50
insomniacc@gmail.com <insomniacc@gmail.com>
2023-03-20 15:54:48 +0000

    Added: Authentication Tests

--- tests/Authentication.Tests.ps1
+++ tests/Authentication.Tests.ps1
@@ -6,13 +6,18 @@ Import-Module "$ModulePath\$ProjectName.psd1" -Force -ErrorAction Stop
 InModuleScope $ProjectName {
     Describe "Authentication" {
         BeforeAll {
+            $RestMethodResponse = Import-Clixml "$PSScriptRoot\MockedResponses\Set-SNOWAuth_oauth.xml"
+
             $Username = 'DummyUsername'
             $Password = 'DummyPassword'
             $Instance = 'DummyInstance'
+            $ClientID = "DummyClientID"
+            $ClientSecret = "DummyClientSecret"
             $SSPassword = $Password | ConvertTo-SecureString -AsPlainText -Force
             $Credential = New-Object PSCredential ($Username, $SSPassword)
             $Command = Get-Command Set-SNOWAuth
-            Set-SNOWAuth -instance $Instance -credential $Credential
+
+            Mock -CommandName Invoke-RestMethod -ParameterFilter { $URI -like "*oauth_token*" -and $Method -eq "POST" } -MockWith { $RestMethodResponse }
         }
 
         Context "Set-SNOWAuth" -tag "Unit" {
@@ -25,14 +30,28 @@ InModuleScope $ProjectName {
                 $Command.Parameters.Keys | Should -Contain $parameter -Because "$parameter is required for $RequiredFor"
             }
 
-            It "Should set authentication in script scope" {
+            It "Should set authentication in script scope [basic]" {
+                Set-SNOWAuth -instance $Instance -credential $Credential
+
                 $script:SNOWAuth | Should -BeOfType Hashtable
                 $script:SNOWAuth.Instance | Should -BeExactly $Instance
                 $script:SNOWAuth.Credential | Should -BeExactly $Credential
                 $script:SNOWAuth.type | Should -BeExactly 'basic'
+                Should -Invoke Invoke-RestMethod -Exactly 0
+            }
+
+            It "Should set authentication in script scope [oauth]" {
+                Set-SNOWAuth -Instance $Instance -Credential $Credential -ClientId $ClientID -ClientSecret $ClientSecret -verbose
+
+                $script:SNOWAuth | Should -BeOfType Hashtable
+                $script:SNOWAuth.Instance | Should -BeExactly $Instance
+                $script:SNOWAuth.ClientID | Should -BeExactly $ClientID
+                $script:SNOWAuth.ClientSecret | Should -BeOfType SecureString
+                $script:SNOWAuth.Token | Should -Not -BeNullOrEmpty
+                $script:SNOWAuth.Expires | Should -BeOfType DateTime
+                $script:SNOWAuth.type | Should -BeExactly 'oauth'
+                Should -Invoke Invoke-RestMethod -Exactly 1
             }
         }
     }
 }
-
-#todo Get-SNOWRITMVariable
